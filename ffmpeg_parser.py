1d7e3be709e474fd631aa4f28bc1ac15d4ca2ec4
DJJO14 <DJJo14@gmail.com>
2021-02-20 21:57:23 +0100

    First test working with changing arguments

--- ffmpeg_parser.py
+++ ffmpeg_parser.py
@@ -1,7 +1,12 @@
 import sys, subprocess, yaml, json
-with open(r'/config/ffmpeg_parser.yaml') as file:
+with open(r'ffmpeg_parser.yaml') as file:
+#with open(r'/config/ffmpeg_parser.yaml') as file:
         config = yaml.load(file, Loader=yaml.FullLoader)
-# print( config )
+print( config )
+
+print( ("h264" in config["codec_decoder"]) )
+print( config["codec_decoder"]["h264"] )
+
 arg = []
 
 for a in sys.argv:
@@ -16,7 +21,7 @@ input_file = None
 print(f"I was called from {arg.pop(0)}", file=sys.stderr)
 while len(arg) > 1:
         a = arg.pop(0)
-        print(f"arg {a} and next {arg[0]}", file=sys.stderr)
+        # print(f"arg {a} and next {arg[0]}", file=sys.stderr)
         if a == "-i":
                 preinput = True
                 input_file = arg.pop(0)
@@ -40,21 +45,120 @@ while len(arg) > 1:
         # if arg[a] == "-c" or arg[a][:len("-codec")] == "-codec":
         #         print(f"found codec { arg[a+1]} preinput:{preinput}")
         #         print(f"switch from input to output")
+##==============================================================
+## Check decoder codec
+##==============================================================
+input_video_decoder = None
+input_video_decoder_arg = None
+
+if "-codec:v:0" in decode_arg:
+        input_video_decoder = decode_arg["-codec:v:0"][0]
+        input_video_decoder_arg = "-codec:v:0"
+elif "-codec:v" in decode_arg:
+        input_video_decoder = decode_arg["-codec:v"][0]
+        input_video_decoder_arg = "-codec:v"
+
+##==============================================================
+## Check input codec
+##==============================================================
+if input_video_decoder == None:
+        print("No decoder found, locking up video codec")
+        ffprobe_cmd = ['ffprobe', '-v', 'quiet', '-show_streams', '-print_format', 'json', input_file]
+        process = subprocess.Popen(ffprobe_cmd, stdout=subprocess.PIPE , cwd=config["ffmpeg_workdir"])
+        stdout = process.communicate()[0]
+        input_file_codec = json.loads(stdout)
+        if "streams" in input_file_codec:
+                #print(input_file_codec["streams"])
+                for s in input_file_codec["streams"]:
+                        #print(s )
+                        if s["codec_type"] == "video" and "codec_name" in s:
+                                print( f"found input codec: {s['codec_name']}")
+                                input_video_decoder = s["codec_name"]
+                else:
+                        print("Could not note find input codec")
 
 
+##==============================================================
+## Replace decoder
+##==============================================================
+if not (input_video_decoder == None and input_video_decoder_arg == None):
+        if input_video_decoder_arg == None:
+                input_video_decoder_arg = "-codec:v"
+                # decode_arg["-codec:v"]
+        print("h264" in config["codec_decoder"])
+        if input_video_decoder in config["codec_decoder"]:
+                old_decoder = input_video_decoder
+                input_video_decoder = config["codec_decoder"][old_decoder]["replace"]
+                print(f"replaceing decoder {old_decoder} with {input_video_decoder}")
 
-# ffprobe_cmd = ['ffprobe', '-v', 'quiet', '-show_streams', '-print_format', 'json', input_file]
-# process = subprocess.Popen(ffprobe_cmd, stdout=subprocess.PIPE)
-# stdout = process.communicate()[0]
-# input_file_codec = json.loads(stdout)
-# if "streams" in input_file_codec:
-#         #print(input_file_codec["streams"])
-#         for s in input_file_codec["streams"]:
-#                 #print(s )
-#                 if "codec_name" in s:
-#                         print( s["codec_name"])
-#       if arg[a].startswith("-"):
-#               print([arg[a], arg[a+1] ])
+                decode_arg[input_video_decoder_arg] = [input_video_decoder]
+
+                for i in config["codec_decoder"][old_decoder]["add"].keys():
+                        if i in decode_arg:
+                                decode_arg[i].append(config["codec_decoder"][old_decoder]["add"][i])
+                        else:
+                                decode_arg[i] = [ config["codec_decoder"][old_decoder]["add"][i] ]
+                
+                for i in config["codec_decoder"][old_decoder]["remove"]:
+                        print(i)
+                        if i in decode_arg:
+                                del decode_arg[i]
+
+                for i in config["codec_decoder"][old_decoder]["remove_encoder"]:
+                        print(i)
+                        if i in encode_arg:
+                                del encode_arg[i]
+                                
+                if len( config["codec_decoder"][old_decoder]["add_start"] ) >= 1:
+                        decode_arg = {**config["codec_decoder"][old_decoder]["add_start"], **decode_arg}
+
+##==============================================================
+## Check encoder codec
+##==============================================================
+input_video_encoder = None
+input_video_encoder_arg = None
+
+if "-codec:v:0" in encode_arg:
+        input_video_encoder = encode_arg["-codec:v:0"][0]
+        input_video_encoder_arg = "-codec:v:0"
+elif "-codec:v" in encode_arg:
+        input_video_encoder = encode_arg["-codec:v"][0]
+        input_video_encoder_arg = "-codec:v"
+
+##==============================================================
+## Replace encoder
+##==============================================================
+if not (input_video_encoder == None and input_video_encoder_arg == None):
+        if input_video_encoder_arg == None:
+                input_video_encoder = "-codec:v"
+                
+        
+        if input_video_encoder in config["codec_encoder"]:
+                old_decoder = input_video_encoder
+                input_video_encoder = config["codec_encoder"][old_decoder]["replace"]
+                print(f"replaceing decoder {old_decoder} with {input_video_encoder}")
+
+                encode_arg[input_video_encoder_arg] = [input_video_encoder]
+                #split arguments in two, to add keys inbetween
+                index_encoder = list(encode_arg.keys()).index(input_video_encoder_arg) + 1
+                encode_arg_firsthalf = dict( list(encode_arg.items())[:index_encoder] )
+                encode_arg_secondhalf = dict( list(encode_arg.items())[index_encoder:] )
+
+                for i in config["codec_encoder"][old_decoder]["add"].keys():
+                        if i in encode_arg_firsthalf:
+                                encode_arg_firsthalf[i].append(config["codec_encoder"][old_decoder]["add"][i])
+                        else:
+                                encode_arg_firsthalf[i] = [ config["codec_encoder"][old_decoder]["add"][i] ]
+
+                encode_arg = {**encode_arg_firsthalf, **encode_arg_secondhalf}
+                
+
+                for i in config["codec_encoder"][old_decoder]["remove"]:
+                        if i in encode_arg:
+                                del encode_arg[i]
+                                
+                if len( config["codec_encoder"][old_decoder]["add_start"] ) >= 1:
+                        encode_arg = {**config["codec_encoder"][old_decoder]["add_start"], **encode_arg}
 
 ##==============================================================
 ## Build output arg
