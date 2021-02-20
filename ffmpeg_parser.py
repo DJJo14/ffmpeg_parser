19e373797d3ca955af36edcc1c1e9ad9b73f0304
DJJO14 <DJJo14@gmail.com>
2021-02-20 22:04:53 +0100

    Clearup code

--- ffmpeg_parser.py
+++ ffmpeg_parser.py
@@ -2,7 +2,7 @@ import sys, subprocess, yaml, json
 with open(r'ffmpeg_parser.yaml') as file:
 #with open(r'/config/ffmpeg_parser.yaml') as file:
         config = yaml.load(file, Loader=yaml.FullLoader)
-print( config )
+# print( config )
 
 print( ("h264" in config["codec_decoder"]) )
 print( config["codec_decoder"]["h264"] )
@@ -62,7 +62,7 @@ elif "-codec:v" in decode_arg:
 ## Check input codec
 ##==============================================================
 if input_video_decoder == None:
-        print("No decoder found, locking up video codec")
+        print("No decoder found, locking up video codec", file=sys.stderr)
         ffprobe_cmd = ['ffprobe', '-v', 'quiet', '-show_streams', '-print_format', 'json', input_file]
         process = subprocess.Popen(ffprobe_cmd, stdout=subprocess.PIPE , cwd=config["ffmpeg_workdir"])
         stdout = process.communicate()[0]
@@ -70,12 +70,13 @@ if input_video_decoder == None:
         if "streams" in input_file_codec:
                 #print(input_file_codec["streams"])
                 for s in input_file_codec["streams"]:
-                        #print(s )
+                        #print(s)
                         if s["codec_type"] == "video" and "codec_name" in s:
                                 print( f"found input codec: {s['codec_name']}")
                                 input_video_decoder = s["codec_name"]
+                                break
                 else:
-                        print("Could not note find input codec")
+                        print("Could not note find input codec", file=sys.stderr)
 
 
 ##==============================================================
@@ -84,8 +85,7 @@ if input_video_decoder == None:
 if not (input_video_decoder == None and input_video_decoder_arg == None):
         if input_video_decoder_arg == None:
                 input_video_decoder_arg = "-codec:v"
-                # decode_arg["-codec:v"]
-        print("h264" in config["codec_decoder"])
+        
         if input_video_decoder in config["codec_decoder"]:
                 old_decoder = input_video_decoder
                 input_video_decoder = config["codec_decoder"][old_decoder]["replace"]
@@ -100,12 +100,10 @@ if not (input_video_decoder == None and input_video_decoder_arg == None):
                                 decode_arg[i] = [ config["codec_decoder"][old_decoder]["add"][i] ]
                 
                 for i in config["codec_decoder"][old_decoder]["remove"]:
-                        print(i)
                         if i in decode_arg:
                                 del decode_arg[i]
 
                 for i in config["codec_decoder"][old_decoder]["remove_encoder"]:
-                        print(i)
                         if i in encode_arg:
                                 del encode_arg[i]
                                 
@@ -134,9 +132,9 @@ if not (input_video_encoder == None and input_video_encoder_arg == None):
                 
         
         if input_video_encoder in config["codec_encoder"]:
-                old_decoder = input_video_encoder
-                input_video_encoder = config["codec_encoder"][old_decoder]["replace"]
-                print(f"replaceing decoder {old_decoder} with {input_video_encoder}")
+                old_encoder = input_video_encoder
+                input_video_encoder = config["codec_encoder"][old_encoder]["replace"]
+                print(f"replaceing decoder {old_encoder} with {input_video_encoder}", file=sys.stderr)
 
                 encode_arg[input_video_encoder_arg] = [input_video_encoder]
                 #split arguments in two, to add keys inbetween
@@ -144,87 +142,49 @@ if not (input_video_encoder == None and input_video_encoder_arg == None):
                 encode_arg_firsthalf = dict( list(encode_arg.items())[:index_encoder] )
                 encode_arg_secondhalf = dict( list(encode_arg.items())[index_encoder:] )
 
-                for i in config["codec_encoder"][old_decoder]["add"].keys():
+                for i in config["codec_encoder"][old_encoder]["add"].keys():
                         if i in encode_arg_firsthalf:
-                                encode_arg_firsthalf[i].append(config["codec_encoder"][old_decoder]["add"][i])
+                                encode_arg_firsthalf[i].append(config["codec_encoder"][old_encoder]["add"][i])
                         else:
-                                encode_arg_firsthalf[i] = [ config["codec_encoder"][old_decoder]["add"][i] ]
+                                encode_arg_firsthalf[i] = [ config["codec_encoder"][old_encoder]["add"][i] ]
 
                 encode_arg = {**encode_arg_firsthalf, **encode_arg_secondhalf}
                 
 
-                for i in config["codec_encoder"][old_decoder]["remove"]:
+                for i in config["codec_encoder"][old_encoder]["remove"]:
                         if i in encode_arg:
                                 del encode_arg[i]
                                 
-                if len( config["codec_encoder"][old_decoder]["add_start"] ) >= 1:
-                        encode_arg = {**config["codec_encoder"][old_decoder]["add_start"], **encode_arg}
+                if len( config["codec_encoder"][old_encoder]["add_start"] ) >= 1:
+                        encode_arg = {**config["codec_encoder"][old_encoder]["add_start"], **encode_arg}
 
 ##==============================================================
 ## Build output arg
 ##================================================================
-test_arguments = False
-if test_arguments:
-        output_list = []
-        # output_list.append(config["ffmpeg_base"])
-        output_list.append("./ffmpeg_parser.py")
 
-        for k, i in decode_arg.items():
-                if i == None:
-                        output_list.append(k)
-                else:
-                        output_list.append(k)
-                        output_list.append(i)
+output_list = []
+output_list.append(config["ffmpeg_base"])
 
-        output_list.append("-i")
-        output_list.append(input_file)
-
-        for k, i in encode_arg.items():
-                if len(i) == 0:
-                        output_list.append(k)
-                else:
-                        for o in i:
-                                output_list.append(k)
-                                output_list.append(o)
-
-        print(output_list, file=sys.stderr)
-
-        if sys.argv == output_list:
-                print("itworks", file=sys.stderr)
-
-        for i in range(len(sys.argv)):
-                if sys.argv[i] == output_list[i]:
-                        print(f"{sys.argv[i]} are eacual", file=sys.stderr)
-                else:
-                        print(f"{sys.argv[i]} != {output_list[i]}", file=sys.stderr)
-else:
-        output_list = []
-        output_list.append(config["ffmpeg_base"])
-
-        for k, i in decode_arg.items():
-                if len(i) == 0:
+for k, i in decode_arg.items():
+        if len(i) == 0:
+                output_list.append(str( k ) )
+        else:
+                for o in i:
                         output_list.append(str( k ) )
-                else:
-                        for o in i:
-                                output_list.append(str( k ) )
-                                output_list.append(str( o ) )
+                        output_list.append(str( o ) )
 
-        output_list.append("-i")
-        output_list.append(input_file)
+output_list.append("-i")
+output_list.append(input_file)
 
-        for k, i in encode_arg.items():
-                if len(i) == 0:
+for k, i in encode_arg.items():
+        if len(i) == 0:
+                output_list.append(str( k ) )
+        else:
+                for o in i:
                         output_list.append(str( k ) )
-                else:
-                        for o in i:
-                                output_list.append(str( k ) )
-                                output_list.append(str( o ) )
-
-        print(output_list, file=sys.stderr)
-
+                        output_list.append(str( o ) )
 
 print(output_list, file=sys.stderr)
-
 
 
 subprocess.run(output_list, cwd=config["ffmpeg_workdir"])
